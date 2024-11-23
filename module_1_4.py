f59889171937e30f1ade30d3be9bbb1c4967dd78
proven_ak <proven@ya.ru>
2024-11-23 22:29:41 +0300

    Модуль 1.4

---
+++ module_1_4.py
@@ -0,0 +1,20 @@
+# Создаем переменную my_string и присваиваем ей значение строки с произвольным текстом
+my_string = input()
+
+# Выводим количество символов введённого текста
+print(len(my_string))
+
+# Выводим строку my_string в верхнем регистре
+print(my_string.upper())
+
+# Выводим строку my_string в нижнем регистре
+print(my_string.lower())
+
+# Удаляем все пробелы
+print(my_string.replace(" ", ""))
+
+# Выводим первый символ строки my_string
+print(my_string[0])
+
+# Выводим последний символ строки my_string
+print(my_string[-1])
