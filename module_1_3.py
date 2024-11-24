a4530a3c745a76c565d938897bcb27be1432c697
proven_ak <proven@ya.ru>
2024-11-24 13:51:32 +0300

    Модуль 1.4

---
+++ module_1_3.py
@@ -0,0 +1,14 @@
+# Создаем переменную для количества выполненных ДЗ
+number_of_completed = 12
+
+# Создаем переменную для количества затраченных часов
+number_of_hours_spent = 1.5
+
+# Создаем переменную для названия курса
+course_name = 'Python'
+
+# Создаем переменную и вычисляем время на одно задание
+time_for_one_task = number_of_hours_spent / number_of_completed
+
+# Выводим на консоль
+print("Курс: ", course_name, ", всего задач:", number_of_completed, ", затрачено часов:", number_of_hours_spent, ", среднее время выполнения ", time_for_one_task, " часа.")
