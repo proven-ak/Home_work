dc27eef458eced88b0bbc71d189dfa8446747956
proven_ak <proven@ya.ru>
2024-12-07 22:40:01 +0300

    add def count_call

---
+++ module_3/module_3_1.py
@@ -0,0 +1,17 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+
+def string_info(string):
+    count_calls()
+    return tup_str
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return
