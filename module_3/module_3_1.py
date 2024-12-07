24f0471047fffc5b51c9889a9ce96a4296ca53c7
proven_ak <proven@ya.ru>
2024-12-08 00:10:55 +0300

    комментарии

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -1,22 +1,29 @@
 calls = 0
 
 
+# Функция подсчета вызовов
 def count_calls():
     global calls
     calls += 1
 
 
+# Функция возвращает кортеж из: длины этой строки, строку в верхнем регистре,
+# строку в нижнем регистре
 def string_info(string):
     count_calls()
     return len(string), string.upper(), string.lower()
 
 
+# Функция принимает два аргумента: строку и список, и
+# возвращает True, если строка находится в этом списке,
+# False - если отсутствует
 def is_contains(string, list_to_search):
     count_calls()
+   # переводим строки списка в верхний регистр
     up_list_to_search = [list_.upper() for list_ in list_to_search]
     return string.upper() in up_list_to_search
 
-
+# Проверка
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
