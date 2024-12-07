bb0c89c4142c301106dd981391a9fd14d56d1e5c
proven_ak <proven@ya.ru>
2024-12-08 00:02:04 +0300

    test module_3_1

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -4,21 +4,21 @@ calls = 0
 def count_calls():
     global calls
     calls += 1
-    return calls
 
 
 def string_info(string):
     count_calls()
-    len_str = len(string)
-    up_str = string.upper()
-    tup_str = (len_str, up_str)
-    return tup_str
+    return len(string), string.upper(), string.lower()
 
 
 def is_contains(string, list_to_search):
     count_calls()
-    if string in list_to_search:
-        retu = True
-    else:
-        retu = False
-    return retu
+    up_list_to_search = [list_.upper() for list_ in list_to_search]
+    return string.upper() in up_list_to_search
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
