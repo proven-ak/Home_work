8e7a07fb62ea6d57a8578e2cb191513ebb9e8ea3
proven_ak <proven@ya.ru>
2024-12-08 13:54:42 +0300

    mod_3_2 проверка суффикса

--- module_3/module_3_2.py
+++ module_3/module_3_2.py
@@ -3,29 +3,40 @@ def send_email(message, recipient, sender = "university.help@gmail.com"):
 
     if "@" not in recipient or "@" not in sender:
         print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+        return
 
-    elif recipient == sender :
+    for sub_suffix in suffix:
+        if sub_suffix not in recipient or sub_suffix not in sender:
+            print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+            return
+
+    if recipient == sender:
         print("Нельзя отправить письмо самому себе!")
 
+    if sender == "university.help@gmail.com":
+        print("Письмо успешно отправлено с адреса ", sender,  "на адрес ", recipient)
 
 
 
 
 
 
- #   for sub_suffix in suffix:
- #       if sub_suffix in
- #               if recipient.find(sub_suffix):
- #                   print()
 
 
-"""
-send_email('Проверка на email', 'urban.fan#mail.ru', sender='urban.info$gmail.com')
-send_email('Проверка на email', 'urban.fan#mail.ru', sender='urban.info@gmail.com')
-send_email('Проверка на email', 'urban.fan@mail.ru', sender='urban.info$gmail.com')
-"""
+send_email('Проверка на "@"', 'urban.fan#mail.ru', sender='urban.info$gmail.com')
+send_email('Проверка на "@"', 'urban.fan#mail.ru', sender='urban.info@gmail.com')
+send_email('Проверка на "@"', 'urban.fan@mail.ru', sender='urban.info$gmail.com')
+
+print("----------------------")
+
+send_email('Проверка suffix', 'urban.fan@mail.ra', sender='urban.info"@"gmail.com')
+send_email('Проверка suffix', 'urban.fan@mail.ru', sender='urban.info@gmail.cam')
+send_email('Проверка suffix', 'urban.fan@mail.ra', sender='urban.info@gmail.cam')
+
+print("----------------------")
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
