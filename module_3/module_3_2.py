395db653c035db241d57a92dcc84e9c6f6b87510
proven_ak <proven@ya.ru>
2024-12-08 12:29:12 +0300

    mod_3_2 добавлен тест для проверки "@"

--- module_3/module_3_2.py
+++ module_3/module_3_2.py
@@ -1,10 +1,20 @@
-
 def send_email(message, recipient, sender = "university.help@gmail.com"):
     suffix = [".com", ".ru", ".net"]
-    if recipient.find("@") or sender.find("@"):
-        for sub_suffix in suffix:
-            if sender.find(sub_suffix):
-                if recipient.find(sub_suffix):
-
-    else:
+    #if recipient.find("@") and sender.find("@"):
+    if "@" not in recipient or "@" not in sender:
         print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+ #        for sub_suffix in suffix:
+ #           if sender.find(sub_suffix):
+ #               if recipient.find(sub_suffix):
+ #                   print()
+
+
+
+send_email('Проверка на email', 'urban.fan#mail.ru', sender='urban.info$gmail.com')
+send_email('Проверка на email', 'urban.fan#mail.ru', sender='urban.info@gmail.com')
+send_email('Проверка на email', 'urban.fan@mail.ru', sender='urban.info$gmail.com')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
