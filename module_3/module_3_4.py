a1f1c55a6d4e0fca55af408f12e7a6937a2469d8
proven_ak <proven@ya.ru>
2024-12-09 15:43:53 +0300

    mod_3_4 corrected if

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -3,22 +3,23 @@
 def single_root_words (root_word, *other_words):
     same_words = []
 
-    same_words = other_words
+    for root in other_words:
 
-    for root in same_words:
+        if root_word.upper() in root.upper() or root.upper() in root_word.upper():
 
-        if root_word in root or root in root_word:
+            same_words.append(root)
 
+            print("+++++", root_word, root)
 
+        else:
 
-
-
-
-
+            print("-----", root_word, root)
 
     return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+print("--------------")
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print("--------------")
 print(result1)
 print(result2)
