58240ca6e6afe1cc2cf2daa943a7ab48a3be2a03
proven_ak <proven@ya.ru>
2024-12-09 21:13:07 +0300

    mod_3_5 add comments

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -3,22 +3,30 @@
 
 def get_multiplied_digits(number):
 
+    # Преобразуем число в строку
     str_number = str(number)
 
+    # Получаем первую цифру числа (в строковом формате).
     first = str_number[0]
 
+    # Если первая цифра равна "0", заменяем её на 1.
     if first == "0":
         return 1
 
+    # Если в числе осталась только одна цифра, возвращаем её как результат.
     if len(str_number) <= 1:
         return int(first)
 
+    # Умножаем первую цифру на результат рекурсивного вызова
+    # функции для оставшейся части числа.
     return int(first) * get_multiplied_digits(int(str_number[1:]))
 
 
+# Тестовые вызовы функции:
 result = get_multiplied_digits(40203)
 result2 = get_multiplied_digits(402030)
 
+# Выводим результаты.
 print(result)
 print(result2)
 
