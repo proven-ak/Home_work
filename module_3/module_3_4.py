86ecc02c835464235991ca7648726a6aa7e23ad8
proven_ak <proven@ya.ru>
2024-12-09 15:57:48 +0300

    mod_3_4 comments

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -1,25 +1,26 @@
 # Самостоятельная работа по уроку Произвольное число параметров.
 
 def single_root_words (root_word, *other_words):
+
+    # Список для хранения слов с совпадением
     same_words = []
 
+    # Перебор всех слов из other_words.
     for root in other_words:
 
+        # Сравнение слов в верхнем регистре для проверки содержания.
         if root_word.upper() in root.upper() or root.upper() in root_word.upper():
 
+            # Если есть совпадение, добавляем слово в список.
             same_words.append(root)
 
-            print("+++++", root_word, root)
-
-        else:
-
-            print("-----", root_word, root)
-
+    # Возвращаем список найденных слов
     return same_words
 
+# Тест функции.
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-print("--------------")
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print("--------------")
+
+# Вывод результатов
 print(result1)
 print(result2)
