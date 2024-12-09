6dcf8263a322749e67d2de12de3eb1005533108a
proven_ak <proven@ya.ru>
2024-12-09 21:12:21 +0300

    mod_3_5 test successful

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -1,12 +1,14 @@
 # Самостоятельная работа по уроку "Рекурсия".
 # Задача "Рекурсивное умножение цифр.
 
-
-def get_multiplied_digits(number ):
+def get_multiplied_digits(number):
 
     str_number = str(number)
 
     first = str_number[0]
+
+    if first == "0":
+        return 1
 
     if len(str_number) <= 1:
         return int(first)
@@ -14,13 +16,10 @@ def get_multiplied_digits(number ):
     return int(first) * get_multiplied_digits(int(str_number[1:]))
 
 
-
 result = get_multiplied_digits(40203)
-
-print(result)
-
 result2 = get_multiplied_digits(402030)
 
+print(result)
 print(result2)
 
 
