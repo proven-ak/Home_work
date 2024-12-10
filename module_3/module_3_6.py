6c5c1cafa098f023db4021868322f4d754d62611
proven_ak <proven@ya.ru>
2024-12-10 17:34:20 +0300

    mod_3_6 add if set

--- module_3/module_3_6.py
+++ module_3/module_3_6.py
@@ -2,10 +2,40 @@
 # Задание "Раз, два, три, четыре, пять .... Это не всё?"
 
 # Входные данные (применение функции):
-data_structure = [
-    [1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+# data_structure = [
+#     [1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
+#     ((), [{(2, 'Urban', ('Urban2', 35))}])
+# ]
+
+data_structure = {1: "a", 2: "b", 3: "c"}         # dict - словарь
+# data_structure = {"a", "b", "c"}                # set - множество
+# data_structure = [1, 2, 3]                      # list - список
+# data_structure = ["a", "b", "c"]                # list - список
 
 
 
+
+
+def calculate_structure_sum(data):
+
+    if type(data) == set:
+
+        print("set", type(data))
+        data = list(data.items())
+        print("set", type(data), data)
+
+
+    # if type(data) == "set":
+    #     print("set", type(data))
+    #
+    # if type(data) == "list":
+    #
+    #     if type(data) == "str":
+    #         print("str", type(data))
+    #
+    #     elif type(data) == "int":
+    #         print("str", type(data))
+
+result = calculate_structure_sum(data_structure)
+
+print(result)
