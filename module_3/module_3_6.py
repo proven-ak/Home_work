b2931c17de27ee40f442d4d5a7834372829ff550
proven_ak <proven@ya.ru>
2024-12-10 19:21:56 +0300

    mod_3_6 add if tuple

--- module_3/module_3_6.py
+++ module_3/module_3_6.py
@@ -2,62 +2,58 @@
 # Задание "Раз, два, три, четыре, пять .... Это не всё?"
 
 # Входные данные (применение функции):
+
 # data_structure = [
 #     [1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
 #     ((), [{(2, 'Urban', ('Urban2', 35))}])
 # ]
 
-#data_structure = {1: "a", 2: "b", 3: "c"}         # dict - словарь
+#data_structure = {1: "a", 2: "b", 3: "c"}       # dict - словарь
 #data_structure = {"a", "b", "c"}                # set - множество
+data_structure = ("a", "b", "c")                # tuple - кортеж
 #data_structure = [1, 2, 3]                      # list - список
-data_structure = ["a", "b", "c"]                # list - список
+#data_structure = ["a", "b", "c"]                # list - список
 
 result = 0
-
+ddd = ("a", "b", "c")
+print(type(ddd))
 
 def calculate_structure_sum(data):
-
     global result
 
     if type(data) == dict:
-
-        print("dict", type(data))
         data = list(data.items())
         print("dict", type(data), data)
 
-    elif type(data) == set:
-
-        print("set", type(data))
+    if type(data) == set:
         data = list(data)
         print("set", type(data), data)
 
+    if type(data) == tuple:
+        data = list(data)
+        print("tuple", type(data), data)
+
     if type(data) == list:
+        print("data", data)
+        copy_data = list(data)
 
-        num_data = [item for item in data if isinstance(item, (int, float))]
-        str_data = [item for item in data if isinstance(item, (str))]
+        for item in copy_data:
 
-        result += len(num_data) + len(str_data)
+            print("item = ", item, "result = ", result)
 
-        print(num_data, str_data)
-        print(len(num_data), len(str_data))
+            if isinstance(item, (int, float, str)):
 
-        print("list", type(data), data)
+                result += 1
+
+            else:
+                calculate_structure_sum(item)
+
+    print(result)
+    print("-------------------")
+
+    return result
 
 
-
-
-
-
-    # if type(data) == "set":
-    #     print("set", type(data))
-    #
-    # if type(data) == "list":
-    #
-    #     if type(data) == "str":
-    #         print("str", type(data))
-    #
-    #     elif type(data) == "int":
-    #         print("str", type(data))
 
 result = calculate_structure_sum(data_structure)
 
