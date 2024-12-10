7778f88455809dc9001ac4052855cdfa38da314a
proven_ak <proven@ya.ru>
2024-12-10 13:36:17 +0300

    mod_3_3 zip replace **

--- module_3/module_3_3.py
+++ module_3/module_3_3.py
@@ -9,20 +9,27 @@ def print_params(a=1, b='строка', c=True):
 print_params(b=25)
 print_params(c=[1, 2, 3])
 
+print("--------------")
 
 # 2. Распаковка параметров.
 # Создаем список с тремя элементами разных типов.
 values_list = [5, True, "Строка"]
-key_list = [1, 2, 3]
+key_list = ["a", "b", "c"]
 
 # Создаем словарь с тремя ключами, соответствующими параметрам
 # функции print_params, и значениями разных типов.
-values_dict = dict(zip(key_list, values_list))
+# values_dict = dict(zip(key_list, values_list))
+values_dict = {key_list[i]: values_list[i] for i in range(len(key_list))}
 
+# Тестируем функцию
+print_params(*values_list)
+print_params(**values_dict)
+
+print("--------------")
 
 # 3. Распаковка + отдельные параметры.
 # Создаем список с двумя элементами разных типов.
 values_list_2 = [54.32, 'Строка']
 
-# Проверяем, работает ли print.
+# Тестируем функцию
 print_params(*values_list_2, c=42)
