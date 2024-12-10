a07800b0af390c87239dd344500464d230c367d6
proven_ak <proven@ya.ru>
2024-12-10 20:09:26 +0300

    mod_3_6 comments

--- module_3/module_3_6.py
+++ module_3/module_3_6.py
@@ -2,48 +2,54 @@
 # Задание "Раз, два, три, четыре, пять .... Это не всё?"
 
 # Входные данные (применение функции):
-
 data_structure = [
     [1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-#data_structure = {1: "a", 2: "b", 3: "c"}       # dict - словарь
-#data_structure = {"a", "b", "c"}                # set - множество
-#data_structure = ("a", "b", "c")                # tuple - кортеж
-#data_structure = [1, 2, 3]                      # list - список
-#data_structure = ["a", "b", "c"]                # list - список
+sum = 0
 
-result = 0
+
+# Объявляем функцию calculate_structure_sum
 def calculate_structure_sum(data):
-    global result
+    # Объявляем глобальную переменную
+    global sum
 
+    # Проверяем тип если словарь преобразовываем в список кортежей
     if type(data) == dict:
         data = list(data.items())
 
+    # Проверяем тип если множество преобразовываем в список
     if type(data) == set:
         data = list(data)
 
+    # Проверяем тип если кортеж преобразовываем в список
     if type(data) == tuple:
         data = list(data)
 
+    # Проверяем тип если список создает копию
     if type(data) == list:
-        copy_data = list(data)
 
-        for item in copy_data:
+        # Перебираем список поэлементно
+        for item in data:
 
+            # Проверяем тип элемента, если число прибавляем к результату
             if isinstance(item, (int, float)):
-                result += item
+                sum += item
 
+            # Проверяем тип элемента, если строка прибавляем длину строки к результату
             elif isinstance(item, (str)):
-                result += len(item)
+                sum += len(item)
 
+            # Иначе повторно обращаемся к функции с этим элементом
             else:
                 calculate_structure_sum(item)
 
-    return result
+    # Возврат результата по завершению функции
+    return sum
 
-
+# Получаем результат
 result = calculate_structure_sum(data_structure)
 
+# Выводим результат на консоль
 print(result)
